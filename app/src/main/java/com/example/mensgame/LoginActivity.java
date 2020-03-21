06cdb2ed4d8d7be7f375074f6a9b416812806a8e
george <george@kenyanrides.com>
2020-03-21 09:13:22 +0300

    Initial commit

---
+++ app/src/main/java/com/example/mensgame/LoginActivity.java
@@ -0,0 +1,25 @@
+package com.example.mensgame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        findViewById(R.id.textViewRegister).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //if user pressed on login
+                //we will open the login screen
+                finish();
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/mensgame/URLS.java
@@ -0,0 +1,8 @@
+package com.example.mensgame;
+
+public class URLS {
+    private static final String ROOT_URL = "http://192.168.100.83/Android/Api.php?apicall=";
+
+    public static final String URL_REGISTER = ROOT_URL + "signup";
+    public static final String URL_LOGIN= ROOT_URL + "login";
+}
